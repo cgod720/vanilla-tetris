267b9480d7a6f496fdbab2823861649f4a92c03e
Carlos Godoy <cgod720@gmail.com>
2019-08-16 10:08:11 -0400

    dropCounter added, T block moves down

--- tetris.js
+++ tetris.js
@@ -3,8 +3,7 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20)
 
-context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.width, canvas.height);
+
 
 const matrix = [
   [0, 0, 0],
@@ -13,6 +12,9 @@ const matrix = [
 ];
 
 const draw = () => {
+  context.fillStyle = '#000';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
   drawMatrix(player.matrix, player.pos)
 }
 
@@ -29,7 +31,20 @@ const drawMatrix = (matrix, offset) => {
   });
 };
 
-const update = () => {
+let dropCounter = 0;
+let dropInterval = 1000;
+
+let lastTime = 0;
+const update = (time = 0) => {
+  const deltaTime = time - lastTime;
+  lastTime = time;
+
+  dropCounter += deltaTime;
+  if(dropCounter > dropInterval){
+    player.pos.y++;
+    dropCounter = 0;
+  }
+
   draw()
   requestAnimationFrame(update)
 }
