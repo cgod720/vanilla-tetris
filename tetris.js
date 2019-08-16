37ee97d110d4052f21e709e91d59080cca50f18a
Carlos Godoy <cgod720@gmail.com>
2019-08-16 17:03:02 -0400

    created buttons

--- tetris.js
+++ tetris.js
@@ -82,6 +82,29 @@ const playerMove = (dir) => {
   }
 }
 
+const playerRotate = (dir) => {
+  rotate(player.matrix, dir);
+}
+
+const rotate = (matrix, dir) => {
+  for(y = 0; y < matrix.length; ++y){
+    for(x = 0; x < y; ++x){
+      [
+        matrix[x][y],
+        matrix[y][x]
+      ] = [
+        matrix[y][x],
+        matrix[x][y]
+      ];
+    }
+  }
+  if(dir > 0){
+    matrix.forEach(row => row.reverse())
+  } else {
+    matrix.reverse();
+  }
+}
+
 let dropCounter = 0;
 let dropInterval = 1000;
 
@@ -115,10 +138,21 @@ document.addEventListener('keydown', (event) => {
   } else if (event.key === 'ArrowDown') {
       playerDrop();
   } else if (event.key === 'ArrowUp') {
-      player.matrix.rotate(-90)
+      playerRotate(1)
+  } else if(event.key === "Q" || event.key === "q"){
+    playerRotate(-1);
   }
-})
+});
 
+document.getElementById('moveLeft').addEventListener('click', (event) => {
+  playerMove(-1);
+});
+document.getElementById('moveRight').addEventListener('click', (event) => {
+  playerMove(1);
+});
+document.getElementById('moveDown').addEventListener('click', (event) => {
+  playerDrop();
+});
 
 // drawMatrix(matrix, {x: 5, y: 5});
 update();
